8f0fa0eb4ce13ff14b840ed990f1eb9f6f3b85ae
MrBenBrittonHVHS <106791155+MrBenBrittonHVHS@users.noreply.github.com>
2025-04-28 21:39:02 +0000

    Apr 28, 2025, 9:39 PM

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,4 +1,18 @@
+/***********************************
+ * Task 01-introduction
+ * Ben Britton Term 2 2025
+ * set up the template for the Javascript Mini skills
+ ***********************************/
 
-
+// Variables
 
 console.log("Running t01_introduction.js");
+
+
+/* 
+* Main Code
+*/
+
+/*
+* Functions
+*/
